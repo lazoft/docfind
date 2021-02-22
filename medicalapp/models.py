d47e46b2f6b64a65004b8ca54b5b30f73bf349a5
MD. SHAHRIYAR HOSSAIN <shahriyar@keemail.me>
2021-02-22 10:33:34 +0600

    Add Databases
    Add databases: Doctor, Hospital, Field, Disease

--- medicalapp/models.py
+++ medicalapp/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.urls import reverse
+from PIL import Image
+from django_resized import ResizedImageField
 
 # Create your models here.
+
+
+class Disease(models.Model):
+    name = models.CharField(max_length=70)
+    parent = models.ForeignKey(
+        'self', blank=True, null=True, related_name='disease', on_delete=models.CASCADE)
+
+    def __self__(self):
+        return self.name
+
+
+class Field(models.Model):
+    name = models.CharField(max_length=70)
+
+    def __self__(self):
+        return self.name
+
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+    image = ResizedImageField(default='default.svg', size=[
+                              300, 300], upload_to='doctor_images')
+    fields = models.ForeignKey(Field, on_delete=models.CASCADE)
+    treats = models.ManyToManyField(Disease)
+
+    def __self__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('', kwargs={'pk': self.pk})
+
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=100)
+    area = models.CharField(max_length=70)
+    division = models.CharField(max_length=30)
+    city = models.CharField(max_length=50)
+    doctors = models.ManyToManyField(Doctor)
+
+    def __self__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('', kwargs={'pk': self.pk})
