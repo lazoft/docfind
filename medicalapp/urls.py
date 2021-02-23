63824f614f254a3a3d4a5a397dad94d1076c110e
MD. SHAHRIYAR HOSSAIN <shahriyar@keemail.me>
2021-02-23 10:52:18 +0600

    Add doctors' list view

--- medicalapp/urls.py
+++ medicalapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import DoctorListView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('doctors/', DoctorListView.as_view(), name='doctor-list')
 ]
--- medicalapp/views.py
+++ medicalapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView
+from .models import Doctor
 
 
 def home(request):
     return render(request, 'medicalapp/home.html')
+
+
+class DoctorListView(ListView):
+    model = Doctor
+    template_name = 'medicalapp/doctors_list.html'
+    context_object_name = 'doctors'
+    ordering = ['name']
+    paginate_by = 10
